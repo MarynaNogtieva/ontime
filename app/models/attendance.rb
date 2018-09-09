f7dec755dd443066ced76112c057199c566484f5
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-09-09 01:39:43 -0400

    refactor form

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,4 +3,12 @@
 class Attendance < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   validates :first_name, :last_name, presence: true
+
+  def allow_clock_in?
+    clock_in_at.blank? && clock_out_at.blank?
+  end
+
+  def allow_clock_out?
+    clock_in_at.present? && clock_out_at.blank?
+  end
 end
